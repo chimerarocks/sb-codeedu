e4c66b4cab8912007af5a53958c510da73ad155b
chimerarocks <joaopedrodslv@gmail.com>
2017-02-24 15:07:51 -0300

    criando ClientInputFilter

---
+++ src/CodeEmailMkt/Application/InputFilter/ClientInputFilter.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace CodeEmailMkt\Application\InputFilter;
+
+use Zend\Filter\StringTrim;
+use Zend\Filter\StripTags;
+use Zend\InputFilter\InputFilter;
+use Zend\Validator\EmailAddress;
+use Zend\Validator\NotEmpty;
+
+class ClientInputFilter extends InputFilter
+{
+	public function __construct()
+	{
+		$this->add([
+			'name'	   => 'name',
+			'required' => true,
+			'filters'  => [
+				['name' => StringTrim::class],
+				['name'	=> StripTags::class]
+			],
+			'validators' => [
+				[
+					'name'		=> NotEmpty::class,
+					'options' 	=> [
+						'message' => [
+							NotEmpty::IS_EMPTY => 'Este campo é requerido'
+						]
+					]
+				]
+			]
+		]);
+
+		$this->add([
+			'name' 		 => 'cpf',
+			'required'	 => true,
+			'filters' 	 => [
+					['name' => StringTrim::class]
+			],
+			'validators' => [
+				[
+					'name'		=> NotEmpty::class,
+					'options' 	=> [
+						'message' => [
+							NotEmpty::IS_EMPTY => 'Este campo é requerido'
+						]
+					]
+				]
+			]
+		])
+
+		$this->add([
+			'name' 		 => 'email',
+			'required'	 => true,
+			'filters' 	 => [
+					['name' => StringTrim::class]
+			],
+			'validators' => [
+				[
+					'name'		=> NotEmpty::class,
+					'options' 	=> [
+						'message' => [
+							NotEmpty::IS_EMPTY => 'Este campo é requerido'
+						]
+					]
+				],
+				[
+					'name'		=> EmailAddress::class,
+					'options' 	=> [
+						'message' => [
+							EmailAddress::INVALID => 'Este email não é válido'
+						]
+					]
+				]
+			]
+		])
+	}
+}
