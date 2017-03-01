55f7168580e51fa8c630c35d1382643ce02a47f9
chimerarocks <joaopedrodslv@gmail.com>
2017-03-01 15:28:06 -0300

    criando entidade user

---
+++ src/CodeEmailMkt/Domain/Entity/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CodeEmailMkt\Domain\Entity;
+
+class User
+{
+	private $id;
+	private $name;
+	private $email;
+	private $password;
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+	public function getEmail()
+	{
+		return $this->email;
+	}
+
+	public function setEmail($email)
+	{
+		$this->email = $email;
+		return $this;
+	}
+
+	public function getPassword()
+	{
+		return $this->password;
+	}
+
+	public function setPassword($password)
+	{
+		$this->password = $password;
+		return $this;
+	}
+}
